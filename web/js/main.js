f65617310456937c5fb4e080d91cea5fd9554700
Will <will@britecore.com>
2018-10-27 16:17:35 -0500

    mobile nav updates

--- web/js/main.js
+++ web/js/main.js
@@ -1,3 +1,7 @@
 (function() {
     $('.c-image-grid__image').simpleLightbox();
+
+    $('.c-nav__mobile-link').on('click', function(e) {
+        $('.c-nav__list').toggleClass('c-nav__list--active');
+    });
 })();
